df6e06fb42e17c7eaf63eb2bb1531eeb82c58cec
VishalLakshmiNarayanan <lvishal1607@gmail.com>
2025-11-22 16:04:14 -0700

    fixed compare_models module

--- tailrisk/utils/__init__.py
+++ tailrisk/utils/__init__.py
@@ -7,11 +7,12 @@ from tailrisk.utils.plotting import (
     plot_predictions_by_quantile,
     plot_residual_distribution
 )
-from tailrisk.utils.validation import print_tail_validation
+from tailrisk.utils.validation import print_tail_validation, compare_models
 
 __all__ = [
     "plot_tail_comparison",
     "plot_predictions_by_quantile",
     "plot_residual_distribution",
     "print_tail_validation",
+    "compare_models",
 ]
